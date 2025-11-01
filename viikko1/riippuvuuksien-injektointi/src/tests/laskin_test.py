d610639b1ac13a953fcf654d6d4c1260f7330e75
westolt <westolt@ad.helsinki.fi>
2025-11-01 13:13:37 +0200

    Exercise 14

--- viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
+++ viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
@@ -21,3 +21,11 @@ class TestLaskin(unittest.TestCase):
         laskin.suorita()
 
         self.assertEqual(io.outputs[0], "Summa: 4")
+
+    def test_kaksi_summaa_oikein(self):
+        io = StubIO(["1", "3", "5", "5", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+        self.assertEqual(io.outputs[1], "Summa: 10")
