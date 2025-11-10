93126f173bb7ee2fa0587cc376aa6ae49249adc8
westolt <westolt@ad.helsinki.fi>
2025-11-10 10:34:16 +0200

    Push bugikorjaus

--- viikko2/index.py
+++ viikko2/index.py
@@ -9,8 +9,8 @@ logger("aloitetaan ohjelma")
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 
-print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
-print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
+print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
+print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
 
 logger("lopetetaan ohjelma")
 print("goodbye!")
