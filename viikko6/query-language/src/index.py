e895ba261dcbb9d913dbf60eaf3be8a91d67ea93
westolt <westolt@ad.helsinki.fi>
2025-12-09 14:03:07 +0200

    Viikko6 exercises 5-6

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,19 +1,24 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+from matchers import QueryBuilder
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("COL"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
+    query = QueryBuilder()
+
+    matcher = (
+    query
+        .one_of(
+            query.plays_in("PHI")
+                .has_at_least(10, "assists")
+                .has_fewer_than(10, "goals"),
+            query.plays_in("EDM")
+                .has_at_least(50, "points")
         )
+        .build()
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -59,3 +59,25 @@ class Or:
 
         return False
 
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self.query_object = query
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self.query_object, PlaysIn(team)))
+    
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasAtLeast(value, attr)))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasFewerThan(value, attr)))
+    
+    def combine_with_and(self, *matchers):
+        return QueryBuilder(And(self.query_object, *matchers))
+    
+    def one_of(self, *queries):
+        matchers = [q.build() for q in queries]
+        return QueryBuilder(Or(*matchers))
+
+    def build(self):
+        return self.query_object
